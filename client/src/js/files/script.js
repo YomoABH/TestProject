f580889acafe1716922c8ffbc12d26434ff70d57
Yomo <sidiropulonikita04@gmail.com>
2022-08-02 00:58:05 +0300

    сделал скрипт

--- client/src/js/files/script.js
+++ client/src/js/files/script.js
@@ -3,3 +3,48 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
+const btn = document.querySelector(".btn")
+const list = document.querySelector(".list")
+
+const state = {
+	users: [],
+}
+
+btn.addEventListener('click', async () => {
+	await getUserForServer();
+})
+
+
+function createElement(users) {
+	`
+	<li class="item">
+		<div class="name">${users.name}</div>
+		<div class="phone">${users.number}</div>
+		<div class="age">${users.age}</div>
+	</li>
+	`
+}
+
+// ? ФУНКЦИЯ ЗАПОЛНЕНИЯ ПОСТА
+function fillPostsList(users) {
+	root.innerHTML = ""
+
+	if (users.length) {
+		users.forEach(user => root.innerHTML += createElement(user))
+	}
+	console.log('отрисовка контента');
+}
+
+
+function getUserForServer() {
+	fetch("", {
+		headers: {
+			"content-type": "application/json; charset=UTF-8"
+		}
+	})
+		.them(res => res.json())
+		.then((users) => {
+			state.users = state.users.concat(users)
+			fillPostsList(state.users)
+		})
+}
