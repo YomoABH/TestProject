a05c8e4f8bd26821ac57789267c334f7a804cac9
David Smyr <swapsanssmyr@gmail.com>
2022-08-02 12:18:29 +0300

    CORS_PERMISIONS_add Nikson's host1

--- client/src/js/files/script.js
+++ client/src/js/files/script.js
@@ -37,7 +37,7 @@ function fillPostsList(users) {
 
 
 function getUserForServer() {
-	fetch("http://127.0.0.1:8000/api/v1/account/", {
+	fetch("http://127.0.0.1:8000/api/v1/ac%D1%81ount/", {
 		headers: {
 			"content-type": "application/json; charset=UTF-8",
 			"Access-Control-Allow-Origin": "http://127.0.0.1:8000/"
