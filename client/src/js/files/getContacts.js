d5c04427d1aa89644219fd431229d16858516684
Nikita Sidiropulo <sidiropulonikita04@gmail.com>
2022-08-04 13:53:32 +0300

    Добавление контактов

---
+++ client/src/js/files/getContacts.js
@@ -0,0 +1,43 @@
+const list = document.querySelector('.list')
+
+const state = {
+	contacts: [],
+}
+
+//* вызов API 
+fetch("http://127.0.0.1:8000/api/v1/ac%D1%81ount/?format=json")
+	.then(res => res.json())
+	.then(contacts => {
+		state.contacts = state.contacts.concat(contacts.humans)
+		pushContact(state.contacts)
+	})
+
+//* Создание шаблона контакта
+const createContact = (contact) => {
+	return (
+		`
+			<li class="item">
+				<div class="flex-wrapper">
+					<div class="">${contact.name}</div>
+					<div class="">${contact.number}</div>
+					<div class="">${contact.age}</div>
+				</div>
+				<div class="btn-panel">
+				<button class="put">put</button>
+				<button class="del">del</button>
+				</div>
+			</li>
+			`
+	)
+}
+
+
+//* Добавление сонтакта в лист
+const pushContact = (contacts) => {
+	list.innerHTML = ""
+	if (contacts.length) {
+		contacts.forEach(contact => {
+			list.innerHTML += createContact(contact)
+		})
+	}
+}
--- client/src/js/files/script.js
+++ client/src/js/files/script.js
@@ -2,63 +2,6 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+import "./getContacts.js"
+import "./addContact.js"
 
-const btn = document.querySelector(".btn")
-const list = document.querySelector(".list")
-
-const state = {
-	users: [],
-}
-
-btn.addEventListener('click', () => {
-	getUserForServer();
-})
-
-
-const createElement = (user) => {
-	return (
-		`
-	<li class="item">
-		<div class="name">${user.name}</div>
-		<div class="phone">${user.number}</div>
-		<div class="age">${user.age}</div>
-	</li>
-	`
-	)
-}
-
-// ? ФУНКЦИЯ ЗАПОЛНЕНИЯ ПОСТА
-function fillPostsList(users) {
-	list.innerHTML = " "
-
-	if (users.length) {
-
-		users.forEach(user => list.innerHTML += createElement(user))
-	}
-}
-
-
-function getUserForServer() {
-	fetch("http://127.0.0.1:8000/api/v1/ac%D1%81ount/?format=json", {
-		headers: {
-			"content-type": "application/json; charset=UTF-8",
-		}
-	})
-		.then(res => res.json())
-		.then((users) => {
-			state.users = state.users.concat(users.humans)
-			fillPostsList(state.users)
-		})
-		.catch(error => list.innerHTML = printErrorList())
-}
-
-const printErrorList = () => {
-	return (
-		`
-	<li class = "error-item">
-		Упс... Произошла ошибка
-	</li>
-`
-	)
-
-}
